d4b0eb88071f0080f2c98cdc94a5908312a80fca
shashanksworld <shashanksworld@gmail.com>
2014-12-28 03:00:10 -0600

    commit all

---
+++ elements/src/com/helper/DrawSimpleGraph.java
@@ -0,0 +1,25 @@
+package com.helper;
+
+import gov.noaa.pmel.sgt.*;
+public class DrawSimpleGraph {
+
+	
+	
+	public static void main(String [] arg)
+	{
+		/*SimpleGraph sg = new SimpleGraph("X v. Y Demo", "x", "y");
+
+		DataSet ds = new DataSet();
+		
+		for (int i = 0; i <= 100; i++) {
+		    ds.addPoint(i, i*i);
+		}
+
+		//add the DataSet to the SimpleGraph
+		sg.addData(ds, "My Key Title");
+
+		//make the graph visible
+		sg.showGraph();
+*/	}
+	
+}
